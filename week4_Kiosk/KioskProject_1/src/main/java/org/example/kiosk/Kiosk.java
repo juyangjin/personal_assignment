b424dba26d8d57ce8ee9a14d7d84c48dd91d5628
joyangjin <wndid2008@gmail.com>
2024-11-22 10:25:10 +0900

    :zap:feat:add Kiosk.class

--- week4_Kiosk/KioskProject_1/src/main/java/org/example/kiosk/Kiosk.java
+++ week4_Kiosk/KioskProject_1/src/main/java/org/example/kiosk/Kiosk.java
@@ -1,29 +1,43 @@
-//package org.example.kiosk;
-//
-//import java.util.Scanner;
-//
-//public class Kiosk {
-//    void start(int num) {
-//        Menu menu = new Menu();
-//        Scanner sc = new Scanner(System.in);
-//        // 반복문 시작
-//        while(true){
-//            menu.category(num);
-//        }
-//
-//        // List와 Menu 클래스 활용하여 상위 카테고리 메뉴 출력
-//
-//        //int num = sc.nextInt();
-//
-//        // 입력 받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
-//        // List<Menu>에 인덱스로 접근하면 Menu만 추출할 수 있겠죠?
-//
-//        // Menu가 가진 List<MenuItem>을 반복문을 활용하여 햄버거 메뉴 출력
-//
-//        //int i = sc.nextInt();
-//        // 입력 받은 숫자가 올바르다면 인덱스로 활용해서 Menu가 가지고 있는 List<MenuItem>에 접근하기
-//        // menu.getMenuItems().get(i); 같은 형식으로 하나씩 들어가서 얻어와야 합니다.
-//        //return true;
-//    }
-//
-//}
+package org.example.kiosk;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Kiosk {
+    Scanner sc = new Scanner(System.in);
+    List<MenuItem> menuItems = new ArrayList<>();
+    public Kiosk(){
+        MenuItem menuItem1 = new MenuItem("1. ShackBurger   | W ",6.9, " | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+        MenuItem menuItem2 = new MenuItem("2. SmokeShack    | W ",8.9," | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+        MenuItem menuItem3 = new MenuItem("3. Cheeseburger  | W ",6.9," | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+        MenuItem menuItem4 = new MenuItem("4. Hamburger     | W ",5.4," | 비프패티를 기반으로 야채가 들어간 기본버거");
+
+        menuItems.clear();
+        menuItems.add(menuItem1);
+        menuItems.add(menuItem2);
+        menuItems.add(menuItem3);
+        menuItems.add(menuItem4);
+    }
+
+    boolean start(boolean end) {
+        while (end){
+            for(MenuItem m : menuItems){
+                System.out.println(m.print());
+            }
+            System.out.println("0. 종료           | 종료");
+            int num = sc.nextInt();
+            sc.nextLine();
+            if(num == 1 || num == 2 || num == 3 || num == 4){
+                num--;
+                System.out.println(menuItems.get(num).print());
+                System.out.println();
+            }else{
+                System.out.println("프로그램을 종료합니다.");
+                end = false;
+            }
+        }
+        return true;
+    }
+
+}
--- week4_Kiosk/KioskProject_1/src/main/java/org/example/kiosk/Main.java
+++ week4_Kiosk/KioskProject_1/src/main/java/org/example/kiosk/Main.java
@@ -8,28 +8,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        MenuItem menuItem1 = new MenuItem("1. ShackBurger   | W ",6.9, " | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-        MenuItem menuItem2 = new MenuItem("2. SmokeShack    | W ",8.9," | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-        MenuItem menuItem3 = new MenuItem("3. Cheeseburger  | W ",6.9," | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-        MenuItem menuItem4 = new MenuItem("4. Hamburger     | W ",5.4," | 비프패티를 기반으로 야채가 들어간 기본버거");
+        boolean end = false;
+        //Menu menu = new Menu("1. Burgers","2. Drinks","3. Desserts");
+        Kiosk kiosk = new Kiosk();
 
-        List<MenuItem> menuItems = new ArrayList<>();
-        menuItems.clear();
-        menuItems.add(menuItem1);
-        menuItems.add(menuItem2);
-        menuItems.add(menuItem3);
-        menuItems.add(menuItem4);
+        System.out.println("시작하려면 start를 입력해주세요.");
+        String start = sc.nextLine();
 
-        for(MenuItem m : menuItems){
-            System.out.println(m.print());
-        }
-        System.out.println("0. 종료           | 종료");
-        int num = sc.nextInt();
-        if(num == 0) {
-            System.out.println("프로그램을 종료합니다.");
-        }else{
-            num--;
-            System.out.println(menuItems.get(num).print());
+        if(start.equals("start")){
+            end = true;
+            kiosk.start(end);
         }
     }
 }
