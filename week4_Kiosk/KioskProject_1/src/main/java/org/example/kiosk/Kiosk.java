a2d5e8449eb84dfdaa3c69c58fa7444d5e5a34fe
joyangjin <wndid2008@gmail.com>
2024-11-26 15:10:30 +0900

    :bulb:style:êadd annotation

--- week4_Kiosk/KioskProject_1/src/main/java/org/example/kiosk/Kiosk.java
+++ week4_Kiosk/KioskProject_1/src/main/java/org/example/kiosk/Kiosk.java
@@ -2,46 +2,41 @@ package org.example.kiosk;
 
 import java.util.Scanner;
 
-public class Kiosk extends Menu{
-    void start(boolean end) {
-        Scanner sc = new Scanner(System.in);
-        while (end){
+public class Kiosk extends Menu{ //Kiosk클래스는 Menu 클래스를 상속한다.
+    void start(boolean end) { //start 메서드 시작되고 매개변수로 boolean 타입을 받는다.
+        Scanner sc = new Scanner(System.in); //값을 스캔받기 위한 스캐너 선언
+        while (end){ // end가 true라면 실행
             System.out.println("[ MAIN MENU ]");
             System.out.println("1. Burgers  |");
             System.out.println("2. Drinks   |");
             System.out.println("3. Desserts |");
-            System.out.println("0. 종료      | 종료");
-            int menu_num = sc.nextInt();
-            sc.nextLine();
+            System.out.println("0. 종료      | 종료"); // 항목 메뉴 출력
+            int menu_num = sc.nextInt(); //원하는 항목을 숫자로 입력받는다.
+            sc.nextLine(); //스캔 오류 방지
 
-            if(menu_num != 0){
-                select(menu_num);
-                for(int i=0;i<getMenuItems().size();i++){
-                    System.out.println(getMenuItems().get(i).print());
+            if(menu_num != 0){ //입력받은 값이 0이 아니라면
+                select(menu_num);//select 메서드를 해당 값을 매개변수로 호출한다.
+                for(int i=0;i<getMenuItems().size();i++){//menuItems값 만큼 반복
+                    System.out.println(getMenuItems().get(i).print()); //참조메서드를 이용하여 menuItems값을 한 줄씩 출력
                 }
-                System.out.println("0. 뒤로가기       | 종료");
+                System.out.println("0. 뒤로가기       | 종료"); //저장된 메뉴값이 아닌 뒤로가기 값을 출력
             }else{
-                System.out.println("프로그램을 종료합니다.");
+                System.out.println("프로그램을 종료합니다.");//항목에 대한 숫자가 0이면 종료메시지 출력
                 end = false;
                 break;
             }
-            int num = sc.nextInt();
-            if(num == 0){
-                System.out.println("프로그램을 종료합니다.");
-                end = false;
-                break;
-            }else{
-                if(getMenuItems().size() >= num){
-                    num--;
-                    System.out.println(getMenuItems().get(num).print());
-                    System.out.println();
+            int num = sc.nextInt();//항목에 대한 상세 메뉴 선택 (숫자)
+            if(num == 0){//뒤로가기 선택
+                start(end);
+            }else{//0이 아니라면 상세 항목 선택을 위해 다음으로 넘어가게 한다.
+                if(getMenuItems().size() >= num){//menuItems 길이와 같거나 작으면 메뉴가 정상 선택된 것이므로
+                    num--;//인덱스 값은 0부터 시작하므로 1을 빼준다.
+                    System.out.println(getMenuItems().get(num).print());//인덱스 값에 맞는 값 출력
+                    System.out.println();//자연스럽게 개행을 한다.
                 }
                 else{
                     System.out.println("선택한 메뉴가 없습니다. 다시 선택해주세요.");
-                    num = sc.nextInt();
-                    num--;
-                    System.out.println(getMenuItems().get(num).print());
-                    System.out.println();
+                    //이 부분 예외처리가 미흡한 부분이 있음. 고칠 필요가 보인다.
                 }
             }
         }
--- week4_Kiosk/KioskProject_1/src/main/java/org/example/kiosk/Menu.java
+++ week4_Kiosk/KioskProject_1/src/main/java/org/example/kiosk/Menu.java
@@ -6,34 +6,35 @@ import java.util.Scanner;
 
 
 public class Menu {
-    private ArrayList<MenuItem> menuItems = new ArrayList<>();
-    MenuItem menuItem1;
+    private ArrayList<MenuItem> menuItems = new ArrayList<>(); //항목별 메뉴를 담을 menuItems 리스트 선언
+    MenuItem menuItem1; //메뉴를 위한 필드들 선언
     MenuItem menuItem2;
     MenuItem menuItem3;
     MenuItem menuItem4;
 
-    public void select(int i) {
-        Scanner sc = new Scanner(System.in);
-        menuItems.clear();
+    public void select(int i) {//매개변수를 int로 받는 메서드
+        Scanner sc = new Scanner(System.in); //스캔을 위한 스캐너 선언
+        menuItems.clear(); //menuItems값을 초기화
         switch (i) {
-            case 1 -> burgers();
+            case 1 -> burgers(); //매개변수 값에 따라서 항목에 맞는 메서드 호출
             case 2 -> drinks();
             case 3 -> desserts();
             default -> {
                 System.out.println("선택한 카테고리가 없습니다. 다시 선택해주세요.");
-                select(sc.nextInt());
+                select(sc.nextInt()); //원하는 case가 아닌 값이 입력되면 예외 메시지가 출력되고 다시 값을 입력받는다.
             }
         }
     }
 
     public void burgers(){
-        menuItems.clear();
+        menuItems.clear();//값 초기화
         menuItem1 = new MenuItem("1. ShackBurger   | W ", 6.9, " | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
         menuItem2 = new MenuItem("2. SmokeShack    | W ", 8.9, " | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
         menuItem3 = new MenuItem("3. Cheeseburger  | W ", 6.9, " | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
         menuItem4 = new MenuItem("4. Hamburger     | W ", 5.4, " | 비프패티를 기반으로 야채가 들어간 기본버거");
         addMenuItems(menuItem1, menuItem2, menuItem3, menuItem4);
     }
+    //MenuItem 생성자로 필드들에 값을 추가하고, 해당 필드들을 addMenuItems에 가변매개변수로 하여 호출
 
     public void desserts(){
         menuItems.clear();
@@ -42,6 +43,7 @@ public class Menu {
         menuItem3 = new MenuItem("3. Cheesecake    | W ", 5.0, " | 부드러운 치즈케이크");
         addMenuItems(menuItem1, menuItem2, menuItem3);
     }
+    //버거 메서드와 동일한 방식으로 새로운 값들이 저장되나 항목이 하나 적다.
 
     public void drinks(){
         menuItems.clear();
@@ -50,14 +52,15 @@ public class Menu {
         menuItem3 = new MenuItem("3. Orange Juice  | W ", 3.0, " | 신선한 오렌지 주스");
         addMenuItems(menuItem1, menuItem2, menuItem3);
     }
+    //버거 메서드와 동일한 방식으로 새로운 값들이 저장되나 항목이 하나 적다.
 
-    public void addMenuItems(MenuItem... item) {
-        menuItems.addAll(Arrays.asList(item));
+    public void addMenuItems(MenuItem... item) {// ...을 사용하여 MenuItem 타입의 가변 매개변수를 받는다.
+        menuItems.addAll(Arrays.asList(item));//매개변수의 모든 값을 menuItems 리스트에 저장
     }
 
     public ArrayList<MenuItem> getMenuItems() {
         return menuItems;
-    }
+    } //menuItems에 대한 참조메서드
 
 
 }
